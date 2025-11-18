bb064ed3b859f2061a3583980c79a3ba0fa38505
vouandzanoeldy@gmail.com <vouandzanoeldy@gmail.com>
2025-11-18 16:11:30 +0100

    fk

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
+
+reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/my-test-output-[hash].xml',
+    toConsole: true,
+  },
+
+e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+
+
 });
